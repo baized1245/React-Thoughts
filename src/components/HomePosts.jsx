202cac62bda65d1848bea2850ef83958cb814555
unknown <you@example.com>
2023-12-26 22:09:40 +0600

    React-Thoughts Beginning

---
+++ src/components/HomePosts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePosts = () => {
+    return (
+        <div>
+            HomePost
+        </div>
+    );
+};
+
+export default HomePosts;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
+
+
+const Navbar = () => {
+    return (
+        <div className='flex items-center justify-between px-6 md:px-[200px] py-4'>
+           
+        </div>
+    );
+};
+
+export default Navbar;
