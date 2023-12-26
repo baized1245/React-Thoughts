05ea67efab7ebafdcd717c3e5476f8b1ca63f15d
unknown <you@example.com>
2023-12-26 22:20:00 +0600

    Navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,18 @@ import { BsSearch } from 'react-icons/bs';
 
 
 const Navbar = () => {
+    const user = true;
     return (
         <div className='flex items-center justify-between px-6 md:px-[200px] py-4'>
-           
+           <h1 className='text-lg md:text-xl font-extrabold'><Link to='/'>Blog Market</Link></h1>
+           <div className="flex justify-center items-center space-x-0">
+            <p><BsSearch/></p>
+            <input className='outline-none px-3' placeholder='Search a post' type="text" name="" id="" />
+           </div>
+           <div className="flex items-center justify-center space-x-2 md:space-x-4">
+            { user ? <h3><Link to='/write'>Write</Link></h3> : <h3 className=''><Link to='/login'>Login</Link></h3>}
+            { user ? <h3>Profile</h3> : <h3 className=''><Link to='/register'>Register</Link></h3>}
+           </div>
         </div>
     );
 };
