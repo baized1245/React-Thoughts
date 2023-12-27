613e5d799c84a28d335801d2787b106b6280fbf5
unknown <you@example.com>
2023-12-27 18:40:17 +0600

    Design Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
   return (
     <div>
       <Navbar/>
             <Routes>
-              <Route exact path='/' element={<Home/>}/>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/register' element={<Register/>}/>
             </Routes>
           <Footer/>  
     </div>
