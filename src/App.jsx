84db40c7e6c3d37e4283013f86dcc5bca005862a
unknown <you@example.com>
2023-12-27 20:29:11 +0600

    Post details page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import PostDetails from './pages/PostDetails';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
             <Routes>
               <Route path='/' element={<Home/>}/>
               <Route path='/login' element={<Login/>}/>
               <Route path='/register' element={<Register/>}/>
+              <Route path='/posts/post/:id' element={<PostDetails/>}/>
             </Routes>
-          <Footer/>  
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import HomePosts from '../components/HomePosts';
-
+import React from "react";
+import HomePosts from "../components/HomePosts";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
-    return (
-        <div className='px-8 md:px-[200px]'>
-          <HomePosts/>
-          <HomePosts/>
-          <HomePosts/>
-          <HomePosts/>
-          <HomePosts/>
-          <HomePosts/>
-          <HomePosts/>
-          <HomePosts/>
-        </div>
-    );
+  return (
+    <>
+    <Navbar/>
+      <div className="px-8 md:px-[200px]">
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+        <HomePosts />
+      </div>
+      <Footer/>
+    </>
+  );
 };
 
 export default Home;
