e55ce8f0138e63674fe1cd4366ea9b5713011671
unknown <you@example.com>
2023-12-27 22:22:19 +0600

    Edit post page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import PostDetails from './pages/PostDetails';
 import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
               <Route path='/register' element={<Register/>}/>
               <Route path='/write' element={<CreatePost/>}/>
               <Route path='/posts/post/:id' element={<PostDetails/>}/>
+              <Route path='/edit/:id' element={<EditPost/>}/>
             </Routes>
     </div>
   );
